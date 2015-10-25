701f09144b8b6e588c99961a8d9e3ee0e2b2fcdc
Maisha <sultana.marium87@gmail.com>
2015-10-25 12:28:18 -0400

    initial commit in master branch

---
+++ README.md
@@ -0,0 +1,2 @@
+# test juint
+with GitHub
---
+++ src/test/java/com/JunitF/JunitTest/JunitFramelog.java
@@ -0,0 +1,28 @@
+package com.JunitF.JunitTest;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+public class JunitFramelog {
+
+	WebDriver dr;
+	@Before
+	public void start(){
+		System.out.println("execution statred");
+	}
+	
+	@Test
+	public void TestFacebook(){
+		dr = new FirefoxDriver();
+		dr.get("https://www.facebook.com");
+	}
+	
+	@After
+	public void end(){
+		dr.quit();
+		System.out.println("execution ended");
+	}
+}
