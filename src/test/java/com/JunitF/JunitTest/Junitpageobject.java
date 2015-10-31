8c52971efbcba98fe8c684f5859734f102ea8bf8
marium sultana <marium sultana>
2015-10-31 13:49:35 -0400

    code added in local branch

--- src/test/java/com/JunitF/JunitTest/Junitpageobject.java
+++ src/test/java/com/JunitF/JunitTest/Junitpageobject.java
@@ -1,23 +1,31 @@
 package com.JunitF.JunitTest;
 
+import java.util.concurrent.TimeUnit;
+
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 
-public class JunitFramelog {
+public class Junitpageobject {
 
 	WebDriver dr;
 	@Before
 	public void start(){
+		dr = new FirefoxDriver();
+		dr.manage().timeouts().implicitlyWait(20, TimeUnit.SECONDS);
 		System.out.println("execution statred");
 	}
 	
 	@Test
 	public void TestFacebook(){
-		dr = new FirefoxDriver();
+	
+		
 		dr.get("https://www.facebook.com");
+		String text = dr.findElement(By.xpath("//h2")).getText();
+		System.out.println("the text is "+ text );
 	}
 	
 	@After
